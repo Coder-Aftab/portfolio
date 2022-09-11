a5c0f5d588cdcdc16d9844e263e6f3bbb8e7390e
Md Aftab <mda552669@gmail.com>
2022-09-12 01:53:49 +0530

    frontend boiler plate ready

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashBoard from './components/dashBoard'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <DashBoard></DashBoard>
+    </div>
+  );
+}
+
+export default App;
